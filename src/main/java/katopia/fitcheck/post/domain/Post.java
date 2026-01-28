975c4bebb5450d96adf2c9572f57e25de528cf5b
rogi-rogi <yrkim6883@gmail.com>
2026-01-28 20:26:30 +0900

    fix: 게시글 삭제 시 댓글이 연쇄 삭제가 안되는 문제 해

--- src/main/java/katopia/fitcheck/post/domain/Post.java
+++ src/main/java/katopia/fitcheck/post/domain/Post.java
@@ -16,6 +16,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OrderBy;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import katopia.fitcheck.comment.domain.Comment;
 import katopia.fitcheck.member.domain.Member;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -48,9 +49,6 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    /*
-    회원탈퇴 시 게시글 하드삭제
-     */
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
@@ -91,6 +89,9 @@ public class Post {
 
     @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<PostLike> postLikes = new LinkedHashSet<>();
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> commentList = new ArrayList<>();
 
     @Builder
     private Post(Member member,
